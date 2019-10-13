38ad0936b33e1006762c2149449a5823da96ca0b
vadzimfiadoryn <vadzimfiadoryn@gmail.com>
2019-10-13 11:06:38 +0300

    feat: add task

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,67 @@
-const Node = require('./node');
-
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this.arr = [];
+        this.length = 0;
+    }
 
-    append(data) {}
 
-    head() {}
+    append(data = 20) {
+        this.arr.push(data);
+        this.length++;
+    }
 
-    tail() {}
+    head() {
+        return this.arr[0];
+    }
 
-    at(index) {}
+    tail() {
+        return this.arr[this.arr.length - 1];
+    }
 
-    insertAt(index, data) {}
+    at(index) {
+        return this.arr[index];
+    }
 
-    isEmpty() {}
+    insertAt(index, data) {
+        return this.arr[index] = data;
+    }
 
-    clear() {}
+    isEmpty() {
+        if (this.arr.length > 0)
+            return false;
+        return true;
+    }
 
-    deleteAt(index) {}
+    clear() {
+        this.arr.length = 1;
+        this.arr[0] = null;
+        this.length = 0;
+    }
 
-    reverse() {}
+    deleteAt(index) {
+        for (let i = index; i < this.arr.length; i++) {
+            this.arr[i] = this.arr[i + 1];
+        }
+        this.arr.length--;
+        this.length--;
+    }
 
-    indexOf(data) {}
+    reverse() {
+        let sizeOfArr = this.arr.length - 1;
+        for (let i = 0; i < (sizeOfArr / 2); i++) {
+            let tempValue = this.arr[i];
+            this.arr[i] = this.arr[sizeOfArr - i];
+            this.arr[sizeOfArr - i] = tempValue;
+        }
+    }
+
+    indexOf(data) {
+        for (let i = 0; i <= this.arr.length; i++) {
+            if (this.arr[i] === data)
+                return i;
+        }
+        return -1;
+    }
 }
 
 module.exports = LinkedList;
